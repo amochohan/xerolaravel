6dda9ab0c69ff49e7408d2a3829e618f9c14087e
tariqbilal <bilaltariq87@gmail.com>
2018-04-04 12:01:42 +0500

    Create XeroItem.php
    To Create new Items through the Xero API

---
+++ Facades/XeroItem.php
@@ -0,0 +1,9 @@
+<?php
+namespace DrawMyAttention\XeroLaravel\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class XeroItem extends Facade
+{
+    protected static function getFacadeAccessor() { return 'XeroItem'; }
+}
